7795a824045bc66eb914328332b3e740f36688c4
Ivana Drábiková <idrabikova@gmail.com>
2016-02-24 13:19:23 +0100

    Initial commit

---
+++ src/ds/monte/carlo/DsMonteCarlo.java
@@ -0,0 +1,13 @@
+package ds.monte.carlo;
+
+/**
+ *
+ * @author Carmen
+ */
+public class DsMonteCarlo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
